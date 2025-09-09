222cf53fe442fd06c0cc04a3cbce32a6ecc24cad
matheusgoncalvesdeoliveira95-cmyk <matheusgoncalvesdeoliveira95@gmail.com>
2025-09-08 22:03:58 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,8 @@
-const cacheName="furufas-cache-v5";
+const cacheName="furufas-cache-v7";
 const filesToCache=["index.html","manifest.json"];
-self.addEventListener("install", e=>{e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));});
-self.addEventListener("fetch", e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener("install", e=>{
+  e.waitUntil(caches.open(cacheName).then(cache=>cache.addAll(filesToCache)));
+});
+self.addEventListener("fetch", e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
